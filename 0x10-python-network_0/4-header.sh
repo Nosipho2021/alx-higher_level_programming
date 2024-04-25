821fba820c5b508b5fca0f606f32f3150bab50fc
Nosipho2021 <lorrainemdk@gmail.com>
2024-04-25 14:41:25 -0700

    update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/python3
-# This script takes a URL as input, sends a GET request to the URL using curl,
-# and displays the body of the response. It also includes a custom header X-School-User-Id with the value 98.
-
-# Check if the number of arguments is exactly 1
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL from the command line argument
-url="$1"
-
-# Use curl to send a GET request to the URL with custom header X-School-User-Id and display the body of the response
-curl -s -H "X-School-User-Id: 98" "$urli"
+#!/bin/bash
+# sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/python3
-# This script takes a URL as input, sends a POST request to the URL using curl,
-# with specified parameters, and displays the body of the response.
-
-# Check if the number of arguments is exactly 1
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL from the command line argument
-url="$1"
-
-# Send a POST request to the URL with specified parameters and display the body of the response
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$url"
+#!/bin/bash
+# sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
